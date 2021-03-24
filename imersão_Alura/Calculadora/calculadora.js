1cfa2aaaa27f65906cf4c8efcddb8dc44d8cdd44
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-03-24 13:30:59 -0600

    calculadora simples

---
+++ imersão_Alura/Calculadora/calculadora.js
@@ -0,0 +1,7 @@
+var primeiroValor = parseInt(prompt("Digite o primeiro valor: "))
+
+var segundoValor =  parseInt(prompt("Digete o segundo valor: "))
+
+var resultado = primeiroValor * segundoValor
+
+document.write("<h2>" + primeiroValor + " x "+ segundoValor + " = " + resultado + "</h2>")
