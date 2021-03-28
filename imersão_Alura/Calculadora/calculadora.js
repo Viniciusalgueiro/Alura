af279e1fc70ce4f9f43f16b966702caa93b2eccc
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-03-27 16:02:01 -0800

    finalizando a Aula 2

--- imersão_Alura/Calculadora/calculadora.js
+++ imersão_Alura/Calculadora/calculadora.js
@@ -2,6 +2,21 @@ var primeiroValor = parseInt(prompt("Digite o primeiro valor: "))
 
 var segundoValor =  parseInt(prompt("Digete o segundo valor: "))
 
-var resultado = primeiroValor * segundoValor
 
-document.write("<h2>" + primeiroValor + " x "+ segundoValor + " = " + resultado + "</h2>")
+var operacao = prompt("Digite 1 para soma , 2 para subtração, 3 para multiplicação e 4 para divisão: ")
+
+if (operacao == 1 ) {
+    var resultado = primeiroValor + segundoValor
+    document.write("<h2>" + primeiroValor + " + "+ segundoValor + " = " + resultado + "</h2>")
+}else if (operacao == 2) {
+    var resultado = primeiroValor - segundoValor
+    document.write("<h2>" + primeiroValor + " - "+ segundoValor + " = " + resultado + "</h2>")
+}else if (operacao == 3){
+    var resultado = primeiroValor * segundoValor
+    document.write("<h2>" + primeiroValor + " * "+ segundoValor + " = " + resultado + "</h2>")
+}else if (operacao == 4){
+    var resultado = primeiroValor / segundoValor
+    document.write("<h2>" + primeiroValor + " / "+ segundoValor + " = " + resultado + "</h2>")
+}else{
+document.write("<h2>" + "Opção invalida" + "</h2>")
+} 
